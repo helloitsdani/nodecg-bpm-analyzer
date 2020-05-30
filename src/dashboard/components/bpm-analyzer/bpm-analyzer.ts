64146f9efad8a68f2906d11431cac559338b39f6
dani quinn sexton <spells@witch.zone>
2020-05-30 01:39:23 +0100

    List available devices

--- src/dashboard/components/bpm-analyzer/bpm-analyzer.ts
+++ src/dashboard/components/bpm-analyzer/bpm-analyzer.ts
@@ -1,9 +1,17 @@
 import * as Polymer from '@polymer/polymer'
 import '@polymer/paper-button/paper-button'
+import '@polymer/paper-dropdown-menu/paper-dropdown-menu'
+import '@polymer/paper-listbox/paper-listbox'
+import '@polymer/paper-item/paper-item'
 
 nodecg.Replicant('bpm', 'nodecg-bpm-analyzer', {
   persistent: false,
 })
+
+const getAudioInputs = async () => {
+  const inputs = await navigator.mediaDevices.enumerateDevices()
+  return inputs.filter(input => input.kind === 'audioinput')
+}
 
 class BPMAnalyzer extends Polymer.PolymerElement {
   static get template() {
@@ -11,6 +19,14 @@ class BPMAnalyzer extends Polymer.PolymerElement {
     <div>
       measure it {{isMeasuring}}
     </div>
+
+    <paper-dropdown-menu label="Devices">
+      <paper-listbox slot="dropdown-content" selected="0">
+        <template is="dom-repeat" items="{{devices}}">
+          <paper-item value="{{item.deviceId}}">{{item.label}}</paper-item>
+        </template>
+      </paper-listbox>
+    </paper-dropdown-menu>
 
     <paper-button raised="" on-tap="startMeasuring">
       Start
@@ -28,7 +44,11 @@ class BPMAnalyzer extends Polymer.PolymerElement {
         type: Boolean,
         value: false,
       },
-      device: {
+      devices: {
+        type: Array,
+        value: [],
+      },
+      selectedDevice: {
         type: String,
         value: '',
       },
@@ -36,7 +56,8 @@ class BPMAnalyzer extends Polymer.PolymerElement {
   }
 
   isMeasuring: boolean = false
-  device: string = ''
+  devices: any[] = []
+  selectedDevice: string = ''
 
   startMeasuring() {
     this.isMeasuring = true
@@ -44,6 +65,15 @@ class BPMAnalyzer extends Polymer.PolymerElement {
 
   stopMeasuring() {
     this.isMeasuring = false
+  }
+
+  async ready() {
+    super.ready()
+
+    // request permissions first, so that listing audio devices returns deviceids
+    await navigator.mediaDevices.getUserMedia({ audio: true })
+
+    this.devices = await getAudioInputs()
   }
 }
 
